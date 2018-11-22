df396388d6173ed29f3dca52483123635ffbac4e
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-11-22 22:04:45 +0100

    [ft-162059433] Create a redflag and Intervention record user interface

--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -3,6 +3,9 @@ const switchToSignin = document.getElementsByClassName("switch-signin")[0];
 const signupForm = document.getElementsByClassName("signup-form")[0];
 const signinForm = document.getElementsByClassName("signin-form")[0];
 
+const report = "http://127.0.0.1:5500/html/report.html";
+const home = "http://127.0.0.1:5500/html/home.html";
+
 let status = "close";
 
 const hamburger = document.getElementsByClassName("hamburger")[0];
@@ -12,6 +15,10 @@ const mainNav = document.getElementsByClassName("main-nav")[0];
 const desktopNav = document.getElementById("desktop-nav");
 const mobileNav = document.getElementById("mobile-nav");
 
+const redFlagRecord = document.getElementById("redflag-record");
+const interventionRecord = document.getElementById("intervention-record");
+
+const recordFrame = document.getElementById("record-frame");
 
 /**
  * Switch to signin or signup form
@@ -54,9 +61,11 @@ const controlHamburger = (event) => {
 
 window.addEventListener("click", controlHamburger);
 
-
+/**
+ * Hamburger menu initializer
+ */
 window.addEventListener("resize", () => {
-  if(screen.availWidth > 999) {
+  if (screen.availWidth > 999) {
     mobileNav.style.display = "none";
     hamburger.src = "../images/menu_icon.png";
     status = "close";
@@ -71,4 +80,63 @@ window.addEventListener("click", (e) => {
   } else if (e.target.className == "outer-modal"){
     document.getElementsByClassName("outer-modal")[0].style.display = "none";
   }
-})
+});
+
+
+/**
+ * Indicates Current records switch
+ * @param {object} event - event object
+ */
+const switchToRecord = (event) => {
+  if (event.target.id == "redflag-record") {
+    redFlagRecord.className = "default";
+    interventionRecord.className = "";
+  } else if (event.target.id == "intervention-record") {
+    redFlagRecord.className = "";
+    interventionRecord.className = "default";
+  }
+}
+
+window.addEventListener("click", switchToRecord);
+
+
+
+window.addEventListener("click", (e) => {
+  if (e.target.className == "picture-evidence") {
+    document.getElementsByClassName("image-enlarge")[0].src = e.target.src;
+    document.getElementsByClassName("outer-modal")[0].style.display = "block";
+  } else if (e.target.className == "outer-modal") {
+    document.getElementsByClassName("outer-modal")[0].style.display = "none";
+  }
+});
+
+
+/**
+ * Hide Comment Area
+ */
+const hideCommentSection = () => {
+  if (window.location.href == report) {
+    const recordFrame = document.getElementById("record-frame");
+    const frameContent = recordFrame.contentDocument;
+    frameContent.getElementsByClassName("comment-heading")[0].remove();
+    frameContent.getElementsByClassName("comment-container")[0].remove();
+    frameContent.getElementsByClassName("partition")[0].remove();
+  }
+}
+recordFrame.addEventListener("load", hideCommentSection);
+
+/**
+ * Increases window length to accommodate content
+ * @param {object} obj - window object
+ */
+const resizeIframe = () => {
+  if (window.location.href == report) {
+    recordFrame.style.height = recordFrame.contentDocument.body.scrollHeight + 50 + 'px';
+  } else if (window.location.href == home) {
+    recordFrame.style.height = recordFrame.contentDocument.body.scrollHeight + 90 + 'px';
+  }
+}
+
+window.addEventListener("load", resizeIframe);
+document.addEventListener("resize", resizeIframe);
+
